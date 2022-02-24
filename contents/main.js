12923eb45f7cfbb314d3376d08997b6acbf75425
Masamitsu MURASE <masamitsu.murase@gmail.com>
2022-02-25 01:18:00 +0900

    Implement sub frame.

--- contents/main.js
+++ contents/main.js
@@ -1,19 +1,9 @@
 (function () {
-    const ROOT_PATH_PREFIX = "md";
-    const DEFAULT_PATH_VALUE = "/" + ROOT_PATH_PREFIX + "/" + "index.md";
     const CONFIG_FILE_NAME = "config.json";
-    const PATH_PARAM_NAME = "path";
     const ATTACHMENT_TYPE = "publishmarkdownreports";
-    const IMAGE_TYPES = {
-        "apng": "image/apng",
-        "gif": "image/gif",
-        "jpg": "image/jpeg",
-        "jpeg": "image/jpeg",
-        "png": "image/png",
-        "svg": "image/svg+xml"
-    };
+    const THIS_URL = new URL(location.href);
 
-    var Path = DevOpsMarked.Path;
+    var gInitializerPromise;
 
 
     class AttachmentLoader {
@@ -22,16 +12,27 @@
             this.projectId = projectId;
             this.planId = build.orchestrationPlan.planId;
             this._attachments = [];
+            this._configData = {};
         }
 
         static async initialize(taskClient, projectId, build) {
             let loader = new AttachmentLoader(taskClient, projectId, build);
             await loader.loadAttachments();
+            await loader.loadConfigData();
             return loader;
         }
 
         async loadAttachments() {
             this._attachments = await this.taskClient.getPlanAttachments(this.projectId, "build", this.planId, ATTACHMENT_TYPE);
+        }
+
+        async loadConfigData() {
+            const configDataBlob = await this.loadContentBlob(CONFIG_FILE_NAME, "text/plain");
+            this._configData = (configDataBlob ? JSON.parse(await configDataBlob.text()) : null);
+        }
+
+        configData() {
+            return this._configData;
         }
 
         find(name) {
@@ -42,108 +43,96 @@
             return this.find(name) !== undefined;
         }
 
-        async contentBlob(name, type) {
+        async loadAttachmentArrayBuffer(name) {
             const attachment = this.find(name);
             if (!attachment) {
                 return null;
             }
             const buffer = await this.taskClient.getAttachmentContent(this.projectId, "build", this.planId,
                 attachment.timelineId, attachment.recordId, ATTACHMENT_TYPE, attachment.name);
+            return buffer;
+        }
+
+        async loadContentBlob(name, type) {
+            const buffer = await this.loadAttachmentArrayBuffer(name);
             return new Blob([buffer], { type: type });
         }
     }
 
-    var scrollToElement = function (elementSelector) {
-        const elem = document.querySelector(elementSelector);
-        if (elem) {
-            elem.scrollIntoView(true);
-            return;
-        }
-
-        const id = elementSelector.slice(1);  // Remove '#'.
-        const aTag = document.querySelector(`a[name="${id}"]`);
-        if (aTag) {
-            aTag.scrollIntoView(true);
-            return;
-        }
+    var sendResponse = function(id, result, data, transfer) {
+        const frame = document.getElementById("markdown_frame");
+        const message = {
+            id: id,
+            result: result,
+            data: data
+        };
+        frame.contentWindow.postMessage(message, THIS_URL.origin, transfer);
     };
 
-    var extractPath = function (url, configData) {
-        if (url.searchParams.has(PATH_PARAM_NAME)) {
-            return Path.fromUrl(url.searchParams.get(PATH_PARAM_NAME));
-        }
-        if (configData.index) {
-            return Path.fromMarkdownAbsolute("/" + configData.index);
-        }
-        return Path.fromUrl(DEFAULT_PATH_VALUE);
+    var sendAttachmentContent = async function (loader, id, path) {
+        const attachmentArrayBuffer = await loader.loadAttachmentArrayBuffer(path);
+        const transfer = attachmentArrayBuffer ? [attachmentArrayBuffer] : undefined;
+        sendResponse(id, true, attachmentArrayBuffer, transfer);
     };
 
-    var loadImageAsync = async function (imageId, loader) {
-        const href = imageId.href;
-        const id = imageId.id;
-        const filePath = Path.fromUrlWithoutSlash(href);
-        const imageExtension = filePath.extension().toLowerCase();
-        if (!IMAGE_TYPES.hasOwnProperty(imageExtension)) {
-            console.warn(`Unknown image type: ${imageExtension}`);
-            return;
-        }
-
-        const imageBlob = await loader.contentBlob(filePath.logPath(), IMAGE_TYPES[imageExtension]);
-        if (!imageBlob) {
-            console.warn(`No image found: ${filePath.logPath()}`);
-            return;
-        }
-
-        const url = URL.createObjectURL(imageBlob);
-        let imageElement = document.querySelector(`#${id}`);
-        if (imageElement) {
-            imageElement.src = url;
-        }
+    var sendConfigData = async function (loader, id) {
+        sendResponse(id, true, loader.configData(), undefined);
     };
 
-    var onBuildChangedCallbackAsync = async function (vsoContext, taskClient, build) {
-        const outputElement = document.querySelector("#publish-markdown-reports-output");
+    var onMessageHandlerAsync = async function (event) {
         try {
-            const loader = await AttachmentLoader.initialize(taskClient, vsoContext.project.id, build);
-            const configDataBlob = await loader.contentBlob(CONFIG_FILE_NAME, "text/plain");
-            const configData = (configDataBlob ? JSON.parse(await configDataBlob.text()) : {});
-
-            const currentUrl = new URL(document.location.href);
-            const path = extractPath(currentUrl, configData);
-            const attachmentBlob = await loader.contentBlob(path.logPath(), "text/plain");
-            if (!attachmentBlob) {
-                console.warn(`Invalid path: ${path.logPath()}`);
-                outputElement.innerHTML = "Load error: file not found.";
+            const expectedOrigin = THIS_URL.origin;
+            if (event.origin != expectedOrigin) {
+                // Just ignore to prevent security issue.
                 return;
             }
 
-            const text = await attachmentBlob.text();
-            const [html, imageIds] = DevOpsMarked.createMarkdownReport(text, path, configData.headerId);
-            outputElement.innerHTML = html;
-
-            const promises = imageIds.map(imageId => loadImageAsync(imageId, loader));
-            await Promise.all(promises);
-
-            if (currentUrl.hash) {
-                scrollToElement(currentUrl.hash);
+            const loader = await gInitializerPromise;
+            const data = event.data;
+            switch (data.type) {
+                case "content":
+                    await sendAttachmentContent(loader, data.id, data.data.name);
+                    break;
+                case "config":
+                    await sendConfigData(loader, data.id);
+                    break;
+                default:
+                    throw new Error(`Unknown type: ${data.type}`);
+                    break;
             }
         } catch (e) {
             console.error(e);
-            outputElement.innerHTML = "Load error.";
+            sendResponse(data.id, false, `${e}`, undefined);
         }
     };
 
-    VSS.init({ usePlatformStyles: false });
-    VSS.ready(function () {
-        const vsoContext = VSS.getWebContext();
-        const config = VSS.getConfiguration();
-        if (!vsoContext || !config) {
-            return;
+    var onBuildChangedCallbackAsync = async function (vsoContext, taskClient, build, resolve, reject) {
+        try {
+            const loader = await AttachmentLoader.initialize(taskClient, vsoContext.project.id, build);
+            resolve(loader);
+        } catch (e) {
+            console.error(e);
+            reject(e);
         }
+    };
 
-        VSS.require(["TFS/DistributedTask/TaskRestClient"], function (taskRestClient) {
-            config.onBuildChanged((build) => {
-                onBuildChangedCallbackAsync(vsoContext, taskRestClient.getClient(), build);
+    gInitializerPromise = new Promise((resolve, reject) => {
+        window.addEventListener("message", function (event) {
+            onMessageHandlerAsync(event);
+        });
+
+        VSS.init({ usePlatformStyles: false });
+        VSS.ready(function () {
+            const vsoContext = VSS.getWebContext();
+            const config = VSS.getConfiguration();
+            if (!vsoContext || !config) {
+                return;
+            }
+
+            VSS.require(["TFS/DistributedTask/TaskRestClient"], function (taskRestClient) {
+                config.onBuildChanged((build) => {
+                    onBuildChangedCallbackAsync(vsoContext, taskRestClient.getClient(), build, resolve, reject);
+                });
             });
         });
     });
